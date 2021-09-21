4c32d78508153d037aa481b8ea9bb1129984bec8
Thiago Guerrero <tgbalera@hotmail.com>
2021-09-20 22:44:31 -0300

    WIP: Fix vuetify and add navbar
    Signed-off-by: Thiago Guerrero <tgbalera@hotmail.com>
    Signed-off-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <v-toolbar flat>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-tabs>
+          <v-tab v-for="(path, i) in paths" :key='i' link :to="path.url" >{{ path.name }}</v-tab>
+        </v-tabs>
+      </v-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    paths: [
+      {name: "Home", url: "/"},
+      {name: "Meu Painel", url: "/painel"}
+    ]
+  })
+
+}
+</script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,30 +1,14 @@
 <template>
-	<div class="home">
-    <v-toolbar flat class="d-flex">
-      <v-toolbar-items class="d-flex">
-
-        <div class="link"> <router-link to="/">Home</router-link> </div>
-        <div class="link"> <router-link to="/painel">Meu Painel</router-link></div>
-        <div class="link"> <router-link to="/disciplinas">Disciplinas</router-link></div>
-
-      </v-toolbar-items>
-  </v-toolbar>
-		
-    <main>
-			<h1></h1>
-		</main>
-	</div>
+  <div class="home">
+    <Navbar />
+  </div>
 </template>
 
 <script>
-export default {
-	name: 'Home'
-}
-</script>
+import Navbar from '../components/Navbar.vue'
 
-<style>
-  .link {
-    margin-right: 8px;
+  export default {
+  components: { Navbar },
+    name: 'Home',
   }
-
-</style>
+</script>
