57d883b378fcf70a3f01e2fa05cd7055229a5934
Thiago Guerrero <tgbalera@hotmail.com>
2021-09-28 22:02:59 -0300

    Finish Board component
    Squashed commit of the following:
    
    commit 6e44bb69c31bf7b40276bf3c5d171656d1f90ef7
    Author: Thiago Guerrero <tgbalera@hotmail.com>
    Date:   Tue Sep 28 22:02:52 2021 -0300
    
        mob next
    
    Signed-off-by: Thiago Guerrero <tgbalera@hotmail.com>
    co-authored-by: Vinicius Frota <viniciuspxfrota@gmail.com>

--- frontend/src/components/Board.vue
+++ frontend/src/components/Board.vue
@@ -1,7 +1,17 @@
 <template>
-  <v-container class="background">
-      <Card v-for="(discipline, index) in disciplines" :key="index" :discipline="discipline" @erase="erase"/>
-  </v-container>
+  <div>
+    <v-sheet class="d-flex justify-center flex-wrap pa-5 board rounded-xl">
+      <template v-if="disciplines.length > 0">
+        <Card v-for="(discipline, index) in disciplines" :key="index" :discipline="discipline" @erase="erase"/>
+      </template>
+      <v-row v-else class="pa-5">
+        <v-col align="center">
+          <span class="font-weight-medium text--disabled">Selecione uma disciplina</span>
+        </v-col>
+      </v-row>
+    </v-sheet>
+    <p class="text-center">{{ captionText }} </p>
+  </div>
 </template>
 
 <script>
@@ -14,6 +24,15 @@ export default {
   components: {
     Card
   },
+  computed: {
+    captionText() {
+      let text = "Disciplinas selecionadas";
+      if (this.disciplines.length == 1)
+        text = "Disciplina selecionada";
+      
+      return this.disciplines.length + ' ' + text;
+    }
+  },
   methods: {
     erase(discipline) {
       this.$emit("erase", discipline);
@@ -23,7 +42,8 @@ export default {
 </script>
 
 <style scoped>
-.background {
+.board {
   background: white;
+  border: 2px solid black;
 }
 </style>
--- frontend/src/views/Painel.vue
+++ frontend/src/views/Painel.vue
@@ -2,7 +2,14 @@
   <v-container>
     <v-row>
       <h1>Painel</h1>
-      <Board :disciplines="disciplines" @erase="eraseDiscipline"/>
+    </v-row>
+    <v-row>
+      <v-col>
+        Input
+      </v-col>
+      <v-col>
+        <Board :disciplines="disciplines" @erase="eraseDiscipline"/>
+      </v-col>
     </v-row>
   </v-container>
 </template>
@@ -11,7 +18,7 @@
 import Board from "@/components/Board.vue"
 export default {
   data: () => ({
-    disciplines: ['MAC0351', 'MAC0110']
+    disciplines: ['MAC0351', 'MAC0110', 'MAC0350', 'MAC0111', 'MAC0352', 'MAC0112', 'MAC0353', 'MAC0113']
   }),
   components: {
     Board
