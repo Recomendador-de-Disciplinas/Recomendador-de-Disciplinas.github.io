f9e8b93f5cf93b13fe12109179e9dea04a7b42da
Thiago Guerrero <tgbalera@hotmail.com>
2021-10-19 21:06:44 -0300

    Add entities structure
    Signed-off-by: Thiago Guerrero <tgbalera@hotmail.com>
    co-authored-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
    co-authored-by: Francisco Eugênio Wernke <franwernke@usp.br>
    co-authored-by: Vinicius Frota <viniciuspxfrota@gmail.com>
    co-authored-by: Daniel Leal <os.leal90@gmail.com>

---
+++ backend/src/main/java/com/poo/backend/entities/Department.java
@@ -0,0 +1,64 @@
+package com.poo.backend.entities;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity(name = "department")
+public class Department {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  private String code;
+  private String name;
+  private String url;
+
+  @OneToMany
+  private List<Discipline> disciplines;
+
+  public Department() {
+  }
+
+  public Department(String name, String code, String url) {
+    this.name = name;
+    this.code = code;
+    this.url = url;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getCode() {
+    return code;
+  }
+
+  public void setCode(String code) {
+    this.code = code;
+  }
+
+  public String getUrl() {
+    return url;
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+  }
+
+  public List<Discipline> getDisciplines() {
+    return disciplines;
+  }
+
+  public void setDisciplines(List<Discipline> disciplines) {
+    this.disciplines = disciplines;
+  }
+
+}
