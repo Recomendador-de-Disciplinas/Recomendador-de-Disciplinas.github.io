4e563c1e8b22cb3baf3af0dc21c9e5cc1109ccbf
Thiago Guerrero <tgbalera@hotmail.com>
2021-10-21 20:28:36 -0300

    Add all repositories
    Signed-off-by: Thiago Guerrero <tgbalera@hotmail.com>
    Co-authored-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>

---
+++ backend/src/main/java/com/poo/backend/repositories/RequisiteByCourseRepository.java
@@ -0,0 +1,9 @@
+package com.poo.backend.repositories;
+
+import com.poo.backend.entities.RequisiteByCourse;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RequisiteByCourseRepository extends JpaRepository<RequisiteByCourse, Long> {
+
+}
---
+++ backend/src/main/java/com/poo/backend/repositories/RequisiteRepository.java
@@ -0,0 +1,8 @@
+package com.poo.backend.repositories;
+
+import com.poo.backend.entities.Requisite;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RequisiteRepository extends JpaRepository<Requisite, Long> {
+}
