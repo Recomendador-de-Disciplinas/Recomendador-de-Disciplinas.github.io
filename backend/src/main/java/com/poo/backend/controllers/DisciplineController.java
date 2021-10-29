5951563cef0a410652352d15e98318ea8abc737c
Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
2021-10-28 21:40:46 -0300

    add fuzzy search to get discipline recommendations
    Signed-off-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
    co-authored-by: Thiago Guerrero <tgbalera@hotmail.com>
    co-authored-by: Daniel Leal <os.leal90@gmail.com>

--- backend/src/main/java/com/poo/backend/controllers/DisciplineController.java
+++ backend/src/main/java/com/poo/backend/controllers/DisciplineController.java
@@ -7,11 +7,10 @@ import java.util.stream.Collectors;
 import com.poo.backend.dto.DepartmentDTO;
 import com.poo.backend.dto.DisciplineDTO;
 import com.poo.backend.dto.UserInputDTO;
-import com.poo.backend.services.DepartmentService;
 import com.poo.backend.services.DisciplineService;
 
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
+import me.xdrop.fuzzywuzzy.FuzzySearch;
+import me.xdrop.fuzzywuzzy.model.ExtractedResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,6 +33,21 @@ public class DisciplineController {
   @PostMapping(path = "/recommendations")
   public List<DisciplineDTO> getRecommendations(@RequestBody UserInputDTO body) {
     List<Long>ids = body.getDepartments().stream().map(DepartmentDTO::getId).collect(Collectors.toList());
-    return disciplineService.findAllByDepartmentsId(ids);
+    List<DisciplineDTO> disciplines = disciplineService.findAllByDepartmentsId(ids);
+
+    List<String> topics = body.getKeywords();
+    List<String> disciplinesNames = disciplines.stream().map(DisciplineDTO::getName).collect(Collectors.toList());
+
+    List<ExtractedResult> results = new ArrayList<>();
+    topics.forEach(keyword -> {
+      List<ExtractedResult> matches = FuzzySearch.extractAll(keyword, disciplinesNames);
+      matches = matches.stream().filter(match -> match.getScore() > 80).collect(Collectors.toList());
+      results.addAll(matches);
+    });
+
+    return results
+            .stream()
+            .map(result -> disciplines.get(result.getIndex()))
+            .collect(Collectors.toList());
   }
 }
