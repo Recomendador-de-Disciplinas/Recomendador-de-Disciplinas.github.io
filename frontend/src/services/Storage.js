a805449c4802d272b7762df6f9da8cd523e1621a
Franwernke <franwernke@usp.br>
2021-11-24 21:55:31 -0300

    Refactor validate in separate file
    co-authored-by: Daniel Leal <os.leal90@gmail.com>

--- frontend/src/services/Storage.js
+++ frontend/src/services/Storage.js
@@ -55,25 +55,3 @@ function getFullObject(allObjects, selecteds) {
     })
   );
 }
-
-function validateFields(userData, backendData) {
-  const { name, courseCode, departments, keywords } = userData;
-  const { allCoursesCode } = backendData;
-  const errors = [];
-
-  if (!name.trim()) errors.push('É necessário informar seu nome');
-
-  if (!allCoursesCode.find((code) => code === courseCode)) {
-    errors.push('Código do curso não existe');
-  }
-
-  if (departments.length === 0)
-    errors.push('É necessário informar ao menos um departamento de interesse');
-  if (keywords.length === 0)
-    errors.push('É necessário informar ao menos um tópico de interesse');
-
-  return {
-    isValid: errors.length === 0,
-    errors,
-  };
-}
--- frontend/src/services/Validate.js
+++ frontend/src/services/Validate.js
@@ -1,27 +1,18 @@
-export default function validateFields(
-  userData,
-  allCoursesCode,
-  type = 'full'
-) {
+export default function validateFields(userData, allCoursesCode) {
   const { name, courseCode, departments, keywords } = userData;
   const errors = [];
 
-  if (type !== 'interests') {
-    if (!name.trim()) errors.push('É necessário informar seu nome');
+  if (!name.trim()) errors.push('É necessário informar seu nome');
 
-    if (!allCoursesCode.find((code) => code === courseCode)) {
-      errors.push('Código do curso não existe');
-    }
+  if (!allCoursesCode.find((code) => code === courseCode)) {
+    errors.push('Código do curso não existe');
   }
 
-  if (type !== 'profile') {
-    if (departments.length === 0)
-      errors.push(
-        'É necessário informar ao menos um departamento de interesse'
-      );
-    if (keywords.length === 0)
-      errors.push('É necessário informar ao menos um tópico de interesse');
-  }
+  if (departments.length === 0)
+    errors.push('É necessário informar ao menos um departamento de interesse');
+
+  if (keywords.length === 0)
+    errors.push('É necessário informar ao menos um tópico de interesse');
 
   return {
     isValid: errors.length === 0,
