902b341bfabf4ffc6cfafa4e0116034e9b7dbd46
Thiago Guerrero <54722457+T-Guerrero@users.noreply.github.com>
2022-01-24 16:32:13 -0300

    Update axios.js

--- frontend/src/plugins/axios.js
+++ frontend/src/plugins/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
+  console.log("AQUI: ", process.env.BACKEND_URL);
   baseURL: process.env.BACKEND_URL || 'http://localhost:8080',
 });
 
